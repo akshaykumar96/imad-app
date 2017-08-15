4e849a871d4b477cac594532a8faa302e53cf9c6
akshaykumar96 <akshaykv1tbm@gmail.com>
2017-08-15 14:44:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 app.get('/article-one', function (req, res) {
-  res.send("coming soon";
+  res.send("coming soon");
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
