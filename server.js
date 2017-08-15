cf89302439e46712499c0232ad70f2ae4be1c456
akshaykumar96 <akshaykv1tbm@gmail.com>
2017-08-15 19:14:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var pool=require('pg').pool;
 
+
+ var config={
+     user:'akshaykv1tbm',
+     database:'akshaykv1tbm',
+     host:'db.imad.hasura-app.io',
+     port:'5432',
+     password: process.env.DB_PASSWORD
+ };
 var app = express();
 app.use(morgan('combined'));
 
@@ -14,6 +23,17 @@ app.get('/ui/style.css', function (req, res) {
 });
 app.get('/article-one', function (req, res) {
   res.send("coming soon");
+});
+app.get('/test-db',function (req,res){
+  pool.query('SELECT * FROM test',function(err,result)
+  {
+      if(err)
+      {res.status(500).send(err.toString());
+      }
+      else
+      { res.send(JSON.stringify(result));
+      }
+  }) ; 
 });
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
